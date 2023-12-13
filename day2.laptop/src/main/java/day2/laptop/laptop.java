31ca479885acf2837d863cde65ec60f383567543
deepanilavazhagan <153614334+deepanilavazhagan@users.noreply.github.com>
2023-12-13 14:20:57 +0530

    Add files via upload
    laptop
    student

---
+++ day2.laptop/src/main/java/day2/laptop/laptop.java
@@ -0,0 +1,26 @@
+package day2.laptop;
+
+public class laptop {
+	public static void main(String[] args) {
+		String laptopName="HP";
+		System.out.println(laptopName);
+		double laptopprice=55000;
+		System.out.println(laptopprice);
+		char firstLetter='H';
+		System.out.println(firstLetter);
+		boolean isDamage=false;
+		System.out.println(isDamage);
+		float frequency=88.6786f;
+		System.out.println(frequency);
+		long imeNumber=3675485874748l;
+		System.out.println(imeNumber);
+		int laptopPiece=32421;
+		System.out.println(laptopPiece);
+		short number=3445;
+		System.out.println(number);
+		byte size=9;
+		System.out.println(size);
+		
+	}
+
+}
---
+++ day2.laptop/src/main/java/student/student.java
@@ -0,0 +1,28 @@
+package student;
+
+public class student {
+	public static void main(String[] args) {
+		String studentName="Deepan";
+		System.out.println(studentName);
+		int rollNumber=1938010057;
+		System.out.println(rollNumber);
+		long mobileNumber=9360110516l;
+		System.out.println(mobileNumber);
+		char studentInitial='k';
+		System.out.println(studentInitial);
+		boolean isStudentpresent=true;
+		System.out.println(isStudentpresent);
+		float studentHeight=6.42f;
+		System.out.println(studentHeight);
+		double studentPercentage=82.10;
+		System.out.println(studentPercentage);
+		short studentweight=80;
+		System.out.println(studentweight);
+		byte studentcgpa=8;
+		System.out.println(studentcgpa);
+		
+	
+		
+	}
+
+}
